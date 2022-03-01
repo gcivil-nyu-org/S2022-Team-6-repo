d70cbf11c3de0a1db4476de86e5c652ef3e14fa6
chinmaykulkarni95 <kulkarni.chinmayrules@gmail.com>
2022-03-01 13:10:54 -0500

    Added Models For Login

--- login/models.py
+++ login/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class UserData(models.Model):
+    firstname = models.CharField(max_length = 100)
+    lastname = models.CharField(max_length = 100)
+    #Should be unique. Need to check if this is unique in backend code
+    username = models.CharField(max_length = 100, primary_key = True)
+    dob = models.DateTimeField()
+    phone = models.IntegerField(default=9999999999)
+    work_address = models.CharField(max_length = 200)
+    home_adress = models.CharField(max_length = 200)
+    is_vacinated = models.BooleanField(default = False)
