8dfaf7044f7a9d29b7515e004a1096eb2712ea80
EashanKaushik <Eashank16@gmail.com>
2022-03-01 14:55:46 -0500

    circle model

--- circle/models.py
+++ circle/models.py
@@ -6,7 +6,7 @@ from login.models import UserData
 class Circle(models.Model):
     circle_id = models.AutoField(primary_key=True)
     circle_name = models.CharField(max_length=100)
-    admin_username = models.CharField(max_length=100)
+    admin_username = models.ForeignKey(UserData, on_delete=models.CASCADE)
     # admin_id = models.CharField(max_length=100, default=0)
     no_of_users = models.IntegerField(default=0)
     created = models.DateTimeField(auto_now_add=True)
@@ -23,13 +23,15 @@ class CircleUser(models.Model):
     is_member = models.BooleanField(default=False)
 
 
-class CirclePolicy(models.Model):
-    circle_id = models.ForeignKey(Circle, on_delete=models.CASCADE)
-    policy_id = models.IntegerField(default=0)
-
-
 class Policy(models.Model):
     policy_id = models.IntegerField(default=0, primary_key=True)
     policy_name = models.CharField(max_length=20)
     policy_desc = models.CharField(max_length=1000)
     policy_level = models.IntegerField(default=0)
+
+
+class CirclePolicy(models.Model):
+    class Meta:
+        unique_together = (('circle_id', 'policy_id'),)
+    circle_id = models.ForeignKey(Circle, on_delete=models.CASCADE)
+    policy_id = models.ForeignKey(Policy, on_delete=models.CASCADE)
