970e8eaa3763ed914e4f74b161e329edaa6091eb
EashanKaushik <Eashank16@gmail.com>
2022-03-01 01:31:07 -0500

    login urls-views

---
+++ login/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('signin/', views.signin, name='signin'),
+    path('signup/', views.signup, name='signup')
+]
--- login/views.py
+++ login/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def index(request):
+    return HttpResponse('index')
+
+
+def signin(request):
+    return HttpResponse('signin')
+
+
+def signup(request):
+    return HttpResponse('signup')
