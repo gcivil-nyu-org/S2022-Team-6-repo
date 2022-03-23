79eaec5b52cca9891a456d8a4ef7e45c2866bd2a
Pranav Singh <46050869+pranavsinghps1@users.noreply.github.com>
2022-03-23 02:26:20 -0400

    Added Installed apps

--- coviguard/settings.py
+++ coviguard/settings.py
@@ -46,6 +46,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'circle',
+    'alert',
+    'login',
+    'monitor',
 ]
 
 MIDDLEWARE = [
