ac836c79ed8c6be9aa7165222e801769243884e0
Pranav Singh <46050869+pranavsinghps1@users.noreply.github.com>
2022-03-23 01:40:25 -0400

    Added Test cases for Monitor,login and circle

---
+++ login/tests.py
@@ -0,0 +1,19 @@
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+from .views import index,signin, signup,error
+
+class TestUrls(SimpleTestCase):
+    def test_index_url_is_resolved(self):
+        url = reverse("login:index")
+        print(resolve(url))
+        self.assertEquals(resolve(url).func,index)
+
+    def test_signin_url_is_resolved(self):
+        url = reverse("login:signin")
+        print(resolve(url))
+        self.assertEquals(resolve(url).func,signin)
+
+    def test_error_url_is_resolved(self):
+        url = reverse("login:error")
+        print(resolve(url))
+        self.assertEquals(resolve(url).func,error)
--- monitor/tests.py
+++ monitor/tests.py
@@ -1,3 +1,12 @@
 # from django.test import TestCase
 
 # Create your tests here.
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+from .views import base
+
+class TestUrls(SimpleTestCase):
+    def test_base_url_is_resolved(self):
+        url = reverse("monitor:user_monitor",args=['asda723h'])
+        print(resolve(url))
+        self.assertEquals(resolve(url).func,base)
