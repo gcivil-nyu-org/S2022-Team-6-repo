ecafa17a92f438c294f12cc52fcb0a898c6c58c8
Pranav Singh <46050869+pranavsinghps1@users.noreply.github.com>
2022-03-29 08:17:58 -0400

    Added Selftracking views test case

--- selftracking/tests/test_views.py
+++ selftracking/tests/test_views.py
@@ -1,15 +1,25 @@
 from django.test import TestCase, Client
 from selftracking.models import SelfTrack
 from django.urls import reverse
+from login.models import UserData
 import datetime
 
 
 class TestViews(TestCase):
     def setUp(self):
-        self.client = Client()
+        self.userdata = UserData.objects.create(
+            firstname="Chinmay",
+            lastname="Kulkarni",
+            password="coviguard",
+            username="EashanKaushik",
+            email="test@gmail.com",
+            dob=datetime.datetime.now(),
+            work_address="1122",
+            home_adress="1122",
+        )
         self.SelfTrackData = SelfTrack.objects.create(
             date_uploaded=datetime.datetime.now(),
-            username="person1",
+            username=self.userdata,
             user_met="met1",
             location_visited="11225",
         )
@@ -18,7 +28,7 @@ class TestViews(TestCase):
             args=["cs55"],
         )
 
-    def add_self_track(self):
+    def test_add_self_track(self):
         response = self.client.get(self.selftrack_url)
-        self.assertEquals(response.status_code, 200)
-        self.assertTemplateUsed(response, "selftracking/self_track.html")
+        self.assertEqual(response.status_code, 302)
+        # self.assertTemplateUsed(response, "selftracking/self_track.html")
